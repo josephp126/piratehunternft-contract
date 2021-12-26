8b70a5834beee8c9cc6da9e741331609f1f5baab
olyjosh <olyjoshone@gmail.com>
2021-12-26 10:09:50 +0100

    Separation of concern for Booty Chest

--- contracts/BootyChest.sol
+++ contracts/BootyChest.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.9;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -14,14 +13,18 @@ import "./IPirateHunters.sol";
 /**
 * Staking, unstaking, claims
 */
+
+interface IUtils {
+    function getSomeRandomNumber(uint _seed, uint _limit) external view returns (uint16);
+    function updateRandomIndex() external;
+}
+
+
 contract BootyChest is Ownable, IERC721Receiver {
 
     using SafeMath for uint256;
 
     bool private _paused = false;
-    uint16 private _randomIndex = 0;
-    uint private _randomCalls = 0;
-    mapping(uint => address) private _randomSource;
 
     struct Stake {
         uint16 tokenId;
@@ -41,6 +44,7 @@ contract BootyChest is Ownable, IERC721Receiver {
     IPirateHunters public pirateHunters;
     IBooty public booty;
     IShop public shop;
+    IUtils public utils;
 
     mapping(uint256 => uint256) public bountyHunterIndices;
     mapping(address => Stake[]) public bountyHunterStake;
@@ -95,18 +99,6 @@ contract BootyChest is Ownable, IERC721Receiver {
     // emergency rescue to allow unstaking without any checks but without $BOOTY
     bool public rescueEnabled = false;
 
-
-    constructor() {
-        // Fill random source addresses
-        _randomSource[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        _randomSource[1] = 0x3cD751E6b0078Be393132286c442345e5DC49699;
-        _randomSource[2] = 0xb5d85CBf7cB3EE0D56b3bB207D5Fc4B82f43F511;
-        _randomSource[3] = 0xC098B2a3Aa256D2140208C3de6543aAEf5cd3A94;
-        _randomSource[4] = 0x28C6c06298d514Db089934071355E5743bf21d60;
-        _randomSource[5] = 0x2FAF487A4414Fe77e2327F0bf4AE2a264a776AD2;
-        _randomSource[6] = 0x267be1C1D684F78cb4F6a176C4911b741E4Ffdc0;
-    }
-
     function paused() public view virtual returns (bool) {
         return _paused;
     }
@@ -133,6 +125,10 @@ contract BootyChest is Ownable, IERC721Receiver {
         shop = IShop(_shop);
     }
 
+    function setShop(address _utils) external onlyOwner {
+        utils = IUtils(_utils);
+    }
+
     function getAccountBountyHunters(address user) external view returns (Stake[] memory) {
         return bountyHunterStake[user];
     }
@@ -144,7 +140,6 @@ contract BootyChest is Ownable, IERC721Receiver {
     function addTokensToStake(address account, uint16[] calldata tokenIds) external {
         require(account == msg.sender || msg.sender == address(pirateHunters), "You do not have a permission to stake tokens");
 
-        // TODO: Reanalyse
         for (uint i = 0; i < tokenIds.length; i++) {
             if (msg.sender != address(pirateHunters)) {
                 // dont do this step if its a mint + stake
@@ -288,44 +283,6 @@ contract BootyChest is Ownable, IERC721Receiver {
         emit BountyHunterClaimed(tokenId, owed, unstake);
     }
 
-//     function _possibleClaimForPirate(uint tokenId) public view returns (uint) {
-
-//         Stake memory stake = pirateStake[msg.sender][pirateIndices[tokenId]];
-
-
-//         require(pirateHunters.ownerOf(stake.tokenId) == address(this), "This NFT does not belong to address");
-//         require(stake.owner == msg.sender, "This NFT does not belong to address");
-//         require(totalBootyEarned < MAXIMUM_GLOBAL_BOOTY, "$BOOTY production stopped");
-//         // require(stake.value > 0, "Token not staked" );
-
-//         // emit Console("After all check", msg.sender, 0);
-//         uint currentTime = block.timestamp;
-
-//         uint x = (currentTime - stake.value) * DAILY_PIRATE_BOOTY_RATE; //((block.timestamp - stake.value) * DAILY_PIRATE_BOOTY_RATE) / 1 days;
-
-//         //((block.timestamp - timestamp) * DAILY_PIRATE_BOOTY_RATE )/ 1 days;
-
-//         // emit Console("After calculation", stake.owner, x);
-
-//         // emit Console("Stake.value", stake.owner, stake.value);
-
-//         return x;
-
-
-// //        // add all extra acquired
-//         // uint pirateReward = 0;
-//         // if(stake.rank == RANK_A){
-//         //     pirateReward = pirateReward_A;
-//         // }else if(stake.rank == RANK_B){
-//         //     pirateReward = pirateReward_B;
-//         // }else{
-//         //     pirateReward = pirateReward_C;
-//         // }
-//         // owed += (pirateReward - stake.xtraReward) + stake.storedReward;
-
-
-//     }
-
     function _possibleClaimForPirate(Stake memory stake) private view returns (uint) {
 
         require(pirateHunters.ownerOf(stake.tokenId) == address(this), "This NFT does not belong to address");
@@ -334,11 +291,11 @@ contract BootyChest is Ownable, IERC721Receiver {
         require(stake.value > 0, "Token not staked" );
 
         // emit Console("After all check", msg.sender, 0);
-        uint owed = 12 ether;
+//        uint owed = 12 ether;
         // uint currentTime = block.timestamp;
-        owed = stake.value;
+//        owed = stake.value;
 //
-//        owed = ((block.timestamp - stake.value) * DAILY_PIRATE_BOOTY_RATE) / 1 days;
+        uint owed = ((block.timestamp - stake.value) * DAILY_PIRATE_BOOTY_RATE) / 1 days;
 //
 //        //((block.timestamp - timestamp) * DAILY_PIRATE_BOOTY_RATE )/ 1 days;
 //
@@ -348,23 +305,21 @@ contract BootyChest is Ownable, IERC721Receiver {
 //
 //
 ////        // add all extra acquired
-//        uint pirateReward = 0;
-//        if(stake.rank == RANK_A){
-//            pirateReward = pirateReward_A;
-//        }else if(stake.rank == RANK_B){
-//            pirateReward = pirateReward_B;
-//        }else{
-//            pirateReward = pirateReward_C;
-//        }
-//        owed += (pirateReward - stake.xtraReward) + stake.storedReward;
+        uint pirateReward = 0;
+        if(stake.rank == RANK_A){
+            pirateReward = pirateReward_A;
+        }else if(stake.rank == RANK_B){
+            pirateReward = pirateReward_B;
+        }else{
+            pirateReward = pirateReward_C;
+        }
+        owed += (pirateReward - stake.xtraReward) + stake.storedReward;
         return owed;
 
     }
 
     function _claimFromPirate(uint16 tokenId, bool unstake) internal returns (uint owed) {
         require(pirateHunters.ownerOf(tokenId) == address(this), "This NFT does not belong to address");
-
-
 
         Stake memory stake = pirateStake[msg.sender][pirateIndices[tokenId]];
 
@@ -379,14 +334,14 @@ contract BootyChest is Ownable, IERC721Receiver {
             owed -= tax;
             _payTaxPirate(tax);
         }else{
-            if (getSomeRandomNumber(tokenId, 100) <= 50) {
+            if (utils.getSomeRandomNumber(tokenId, 100) <= 50) {
                 uint burn = (PERCENTAGE_OF_ROBBED_BURN_PIRATE * owed) /100;
                 _payTaxPirate(owed-burn); // pay 60% of what is robbed
                 mintAndBurn(burn); // burn 40%
                 emit BootyBurned(tokenId, PERCENTAGE_OF_ROBBED_BURN_PIRATE, owed, 0 );
                 owed = 0;
             }
-            updateRandomIndex();
+            utils.updateRandomIndex();
         }
 
         if (unstake) {
@@ -499,80 +454,38 @@ contract BootyChest is Ownable, IERC721Receiver {
     }
 
     function effectRankUp(uint tokenId, uint newRank) external {
-        // require(msg.sender == address(shop), "Unauthorized");
-        // Stake memory stake = pirateStake[msg.sender][pirateIndices[tokenId]];
-        // if(newRank == RANK_B){
+         require(msg.sender == address(shop), "Unauthorized");
+         Stake memory stake = pirateStake[msg.sender][pirateIndices[tokenId]];
+         if(newRank == RANK_B){
 
-        //     // calculate carry over shared reward
-        //     uint share = (pirateReward_C - stake.xtraReward)/totalPirateRank_C;
-        //     pirateStake[msg.sender][pirateIndices[tokenId]] = Stake({
-        //         owner: msg.sender,
-        //         tokenId: uint16(tokenId),
-        //         value: uint80(block.timestamp),// ,
-        //         xtraReward: uint80(pirateReward_B),
-        //         storedReward: share,
-        //         rank: RANK_B
-        //     }); // reset stake
+             // calculate carry over shared reward
+             uint share = (pirateReward_C - stake.xtraReward)/totalPirateRank_C;
+             pirateStake[msg.sender][pirateIndices[tokenId]] = Stake({
+                 owner: msg.sender,
+                 tokenId: uint16(tokenId),
+                 value: uint80(block.timestamp),// ,
+                 xtraReward: uint80(pirateReward_B),
+                 storedReward: share,
+                 rank: RANK_B
+             }); // reset stake
 
-        //     totalPirateRank_C-=1;
-        //     totalPirateRank_B+=1;
-        // }else if(newRank == RANK_A){
+             totalPirateRank_C-=1;
+             totalPirateRank_B+=1;
+         }else if(newRank == RANK_A){
 
-        //     uint share = (pirateReward_B - stake.xtraReward)/totalPirateRank_B;
-        //     pirateStake[msg.sender][pirateIndices[tokenId]] = Stake({
-        //         owner: msg.sender,
-        //         tokenId: uint16(tokenId),
-        //         value: uint80(block.timestamp),// ,
-        //         xtraReward: uint80(pirateReward_A),
-        //         storedReward: share,
-        //         rank: RANK_A
-        //     });
+             uint share = (pirateReward_B - stake.xtraReward)/totalPirateRank_B;
+             pirateStake[msg.sender][pirateIndices[tokenId]] = Stake({
+                 owner: msg.sender,
+                 tokenId: uint16(tokenId),
+                 value: uint80(block.timestamp),// ,
+                 xtraReward: uint80(pirateReward_A),
+                 storedReward: share,
+                 rank: RANK_A
+             });
 
-        //     totalPirateRank_B -=1;
-        //     totalPirateRank_A +=1;
-        // }
-    }
-
-    function updateRandomIndex() internal {
-        _randomIndex += 1;
-        _randomCalls += 1;
-        if (_randomIndex > 6) _randomIndex = 0;
-    }
-
-    function getSomeRandomNumber(uint _seed, uint _limit) internal view returns (uint16) {
-        uint extra = 0;
-        for (uint16 i = 0; i < 7; i++) {
-            extra += _randomSource[_randomIndex].balance;
-        }
-
-        uint random = uint(
-            keccak256(
-                abi.encodePacked(
-                    _seed,
-                    blockhash(block.number - 1),
-                    block.coinbase,
-                    block.difficulty,
-                    msg.sender,
-                    extra,
-                    _randomCalls,
-                    _randomIndex
-                )
-            )
-        );
-
-        return uint16(random % _limit);
-    }
-
-    function changeRandomSource(uint _id, address _address) external onlyOwner {
-        _randomSource[_id] = _address;
-    }
-
-    function shuffleSeeds(uint _seed, uint _max) external onlyOwner {
-        uint shuffleCount = getSomeRandomNumber(_seed, _max);
-        _randomIndex = uint16(shuffleCount);
-        for (uint i = 0; i < shuffleCount; i++) {
-            updateRandomIndex();
-        }
+             totalPirateRank_B -=1;
+             totalPirateRank_A +=1;
+         }
     }
 
     function onERC721Received(
